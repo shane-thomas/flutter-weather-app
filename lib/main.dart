a46393a76335539fc251cda0c435cbc6fa5e9884
Shane Shaji Thomas <shaneshaji10@gmail.com>
2023-05-01 15:49:19 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 //Shane Shaji Thomas 22BCE3649
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'homescreen.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Weather App',
     home: HomeScreen(),
     debugShowCheckedModeBanner: false,
   ));
